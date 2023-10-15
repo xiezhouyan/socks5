5be31c701be13083f4eb3b815f649d59daeca594
xiezhouyan <499749021@qq.com>
2023-10-15 23:03:15 +0800

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -69,7 +69,7 @@ function installWordPress()
     cd /var/www/${domain};
     wp core download --allow-root
     wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --dbhost=127.0.0.1 --dbprefix=wp --allow-root
-    wp core install --url=$domain --title=$domain --admin_user=admin --admin_password=admin@qwe!123 --admin_email=wp_admin@163.com
+    wp core install --url=$domain --title=$domain --admin_user=admin --admin_password=admin@qwe!123 --admin_email=wp_admin@163.com --allow-root 
     wp theme install hello-elementor --allow-root 
     wp theme install botiga --activate  --allow-root 
     wp plugin --install https://downloads.wordpress.org/plugin/woocommerce.8.2.0.zip --activate --allow-root 
