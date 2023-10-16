f4eecb8f9f3e41ddd334f11cde78fdad12a3b718
xiezhouyan <499749021@qq.com>
2023-10-16 23:45:49 +0800

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -88,7 +88,7 @@ function config()
     dbname=${dbkey}
     dbuser=${dbkey}
     # dbpass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
-    dbpass='admin@qwe!123'
+    dbpass='admin@qwe'
     mysql -u root <<EOF
 DELETE FROM mysql.user WHERE User='${dbuser}';
 CREATE DATABASE $dbname default charset utf8mb4;
