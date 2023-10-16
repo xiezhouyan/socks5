f80b59bf5023fe3b26845c7b82325d144793ffc1
xiezhouyan <499749021@qq.com>
2023-10-17 00:04:31 +0800

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -94,8 +94,6 @@ DROP user IF EXISTS ${dbuser};
 CREATE DATABASE IF NOT EXISTS $dbname default charset utf8mb4;
 CREATE USER ${dbuser}@'%' IDENTIFIED BY ${dbpass};
 GRANT ALL PRIVILEGES ON ${dbname}.* to ${dbuser}@'%';
-CREATE USER ${dbuser}@'localhost' IDENTIFIED BY '${dbpass}';
-GRANT ALL PRIVILEGES ON ${dbname}.* to ${dbuser}@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
