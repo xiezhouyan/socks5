c5a8534d7797392f17a5f2e9a0080c0124fc8233
xiezhouyan <499749021@qq.com>
2023-10-23 23:05:55 +0800

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -76,6 +76,7 @@ function installWordPress()
     wp config set ALLOW_UNFILTERED_UPLOADS true --raw --allow-root
     wp core install --url=$domain --title=$domain --admin_user=admin --admin_password=admin@qwe!123 --admin_email=wp_admin@163.com --allow-root 
     wp plugin install https://downloads.wordpress.org/plugin/woocommerce.7.7.2.zip --activate --allow-root 
+    wp plugin install woocommerce-paypal-payments --activate --allow-root 
     wp theme install botiga  --activate --allow-root
     wp rewrite structure '/%postname%/' --allow-root
     wp eval '
