5c0f2fe873dcb9cbedcbd8cfa561e5f5d1a777e5
xiezhouyan <499749021@qq.com>
2023-10-23 21:29:35 +0800

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -75,7 +75,6 @@ function installWordPress()
     wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --dbhost=127.0.0.1 --dbprefix=wp --allow-root
     wp core install --url=$domain --title=$domain --admin_user=admin --admin_password=admin@qwe!123 --admin_email=wp_admin@163.com --allow-root 
     wp plugin install https://downloads.wordpress.org/plugin/woocommerce.7.7.2.zip --activate --allow-root 
-    wp plugin install https://raw.githubusercontent.com/xiezhouyan/socks5/master/fifu-6.2.2.zip --activate --allow-root 
     wp theme install botiga  --activate --allow-root
     wp rewrite structure '/%postname%/' --allow-root
     wp eval '
